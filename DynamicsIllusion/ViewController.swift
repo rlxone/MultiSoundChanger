85ffcf258ba1dd0236ed7286bbd5b116f0e40562
rlxone <reflexxblr@gmail.com>
2017-04-02 17:59:10 +0300

    Initial Commit

---
+++ DynamicsIllusion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DynamicsIllusion
+//
+//  Created by sdd on 02.04.17.
+//  Copyright © 2017 mityny. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
